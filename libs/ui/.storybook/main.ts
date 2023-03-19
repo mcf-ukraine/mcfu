352e3a7d0f161a1bc10097d5aeb2bc38f315940f
Roman Batsenko <rbatsenko@gmail.com>
2023-03-19 22:00:11 +0000

    Add Storybook for ui, upgrade NX

---
+++ libs/ui/.storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+
+const config: StorybookConfig = {
+  core: {},
+  stories: ["../src/lib/**/*.mdx", "../src/lib/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  async viteFinal(config: any) {
+    return mergeConfig(config, {});
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+module.exports = config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
