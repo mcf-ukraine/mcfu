95e140fb75c0ad8ddf5e36b791ff10db1c83aede
Roman Batsenko <rbatsenko@gmail.com>
2023-03-23 16:30:27 +0100

    Fix configs

--- apps/membership-dashboard/next.config.mjs
+++ apps/membership-dashboard/next.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 !process.env.SKIP_ENV_VALIDATION && (await import("./env.mjs"));
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require("@nrwl/next/plugins/with-nx");
+import { withNx } from "@nrwl/next/plugins/with-nx";
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
