37a4f6d2cf59465e1053699e5d42f8977f4da056
Roman Batsenko <rbatsenko@gmail.com>
2023-04-20 18:38:33 +0200

    chore: show toasts on the top right corner on mobile

--- apps/member-panel/vitest.setup.ts
+++ apps/member-panel/vitest.setup.ts
@@ -7,6 +7,22 @@ expect.extend(matchers);
 
 vi.mock("next/router", () => require("next-router-mock"));
 
+beforeAll(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // Deprecated
+      removeListener: vi.fn(), // Deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+});
+
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
--- libs/ui/src/lib/components/Toaster/Toaster.tsx
+++ libs/ui/src/lib/components/Toaster/Toaster.tsx
@@ -1,9 +1,15 @@
 import { Toaster as RHTToaster } from "react-hot-toast";
+import { useMediaQuery } from "usehooks-ts";
 
-export const Toaster = () => (
-  <RHTToaster
-    position="bottom-right"
-    reverseOrder={false}
-    toastOptions={{ custom: { duration: 6000 } }}
-  />
-);
+export const Toaster = () => {
+  const isMobile = useMediaQuery("(max-width: 639px)");
+  const tosterPosition = isMobile ? "top-right" : "bottom-right";
+
+  return (
+    <RHTToaster
+      position={tosterPosition}
+      reverseOrder={false}
+      toastOptions={{ custom: { duration: 6000 } }}
+    />
+  );
+};
